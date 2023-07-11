e695969c9205888181f4d8cc2559a79a1dd4c705
Patricia Byrro <pbbyrro@gmail.com>
2023-07-11 14:03:33 -0300

    Move files

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,5 @@
-const leapYears = function() {
-
+const leapYears = function(number) {
+    return number % 4 === 0 && (number % 100 !== 0 || number % 400 === 0 )
 };
 
 // Do not edit below this line
